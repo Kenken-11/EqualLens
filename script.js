61a96a55205a1d86338e921bcbf53695654e1d4b
Kenken-11 <78901818+Kenken-11@users.noreply.github.com>
2023-12-17 11:06:07 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('page1Button').addEventListener('click', function() {
+    window.location.href = './index.html';
+});
+
+document.getElementById('page2Button').addEventListener('click', function() {
+    window.location.href = './index2.html';
+});
